7fa16004ea1293df9c8e7fa877249b71293a53eb
Jimena Hernández <her21199@uvg.edu.gt>
2024-02-28 23:57:28 -0600

    Update corto1.py
    Co-Authored-By: Mark Albrand <mark.albrand@protonmail.com>
    Co-Authored-By: Melissa Pérez <84192851+melissaperez09@users.noreply.github.com>

--- corto1.py
+++ corto1.py
@@ -4,7 +4,7 @@ import numpy as np
 from IPython.display import clear_output
 import time 
 
-env = gym.make('FrozenLake-v1', is_slippery=True, render_mode='human')
+env = gym.make('FrozenLake-v1', is_slippery=True)
 
 no_states = env.observation_space.n
 no_actions = env.action_space.n
@@ -12,9 +12,9 @@ no_actions = env.action_space.n
 Q = np.zeros((no_states, no_actions))
 
 # Parámetros del algoritmo
-iteraciones = 1000
-alpha = 0.5
-gamma = 0.9
+iteraciones = 100000
+alpha = 0.8
+gamma = 0.95
 
 print("Entreno del agente")
 print ('Q-table antes:')
@@ -27,13 +27,9 @@ for i in range(iteraciones):
     while not finish:
         # estado viene en este formato: (0, {'prob': 1})
         # elegir la acción con mayor valor en el estado actual
-        if np.max(Q[estado]) > 0:
-            accion = np.argmax(Q[estado])
-            
-        else:
-            accion = env.action_space.sample()
-
-        estado_nuevo, recompensa, finish, info, _ = env.step(accion)
+        accion = np.argmax(Q[estado,:] + np.random.randn(1,env.action_space.n)*(1./(i+1)))
+    
+        estado_nuevo, recompensa, finish, _, _ = env.step(accion)
 
         Q[estado, accion] = Q[estado, accion] + alpha * (recompensa + gamma * np.max(Q[estado_nuevo]) - Q[estado, accion])
         estado = estado_nuevo
@@ -44,6 +40,8 @@ print(Q)
 
 # Test
 print("Test del agente")
+
+env = gym.make('FrozenLake-v1', is_slippery=True, render_mode='human')
 
 state = env.reset()[0]
 done = False
